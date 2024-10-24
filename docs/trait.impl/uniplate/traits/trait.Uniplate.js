a80c76c9bfb8403f0f6926267db9258cccc99bcd
ozgurakgun <ozgurakgun@gmail.com>
2024-10-24 22:44:51 +0000

    Actions: Documentation for

--- docs/trait.impl/uniplate/traits/trait.Uniplate.js
+++ docs/trait.impl/uniplate/traits/trait.Uniplate.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["conjure_core",[["impl Uniplate for <a class=\"enum\" href=\"conjure_core/ast/enum.Constant.html\" title=\"enum conjure_core::ast::Constant\">Constant</a>"],["impl Uniplate for <a class=\"enum\" href=\"conjure_core/ast/enum.Expression.html\" title=\"enum conjure_core::ast::Expression\">Expression</a>"]]]]);
+    var implementors = Object.fromEntries([["conjure_core",[["impl Uniplate for <a class=\"enum\" href=\"conjure_core/ast/enum.Constant.html\" title=\"enum conjure_core::ast::Constant\">Constant</a>"],["impl Uniplate for <a class=\"enum\" href=\"conjure_core/ast/enum.Expression.html\" title=\"enum conjure_core::ast::Expression\">Expression</a>"],["impl Uniplate for <a class=\"struct\" href=\"conjure_core/metadata/struct.Metadata.html\" title=\"struct conjure_core::metadata::Metadata\">Metadata</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[306]}
+//{"start":57,"fragment_lengths":[463]}
